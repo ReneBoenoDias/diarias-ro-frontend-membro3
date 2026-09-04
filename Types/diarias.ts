89d912a3c139713275c3b5d0ea2b4afcd4248bb2
Rene Dias <renedelimaboenodias@gmail.com>
2026-09-04 14:37:51 -0400

    Cria tipagens da calculadora de diarias

---
+++ Types/diarias.ts
@@ -0,0 +1,38 @@
+export enum CategoriaCargo {
+  GovernadorVice = 1,
+  Secretarios = 2,
+  GerenciaSuperior = 3,
+  GerenciaIntermediariaDemais = 4,
+}
+
+export enum TipoViagem {
+  Nacional = 0,
+  Internacional = 1,
+}
+
+export enum TipoRegra {
+  Padrao = "Padrao",
+  CursoLongo = "CursoLongo",
+  AgenteMultiplicador = "AgenteMultiplicador",
+  JOER = "JOER",
+}
+
+export interface RequisicaoCalculoDiaria {
+  categoria: CategoriaCargo;
+  dataHoraInicio: string;
+  dataHoraFim: string;
+  tipo: TipoViagem;
+  cotacaoDolar: number | null;
+  hospedagemInclusa: boolean;
+  custosTotaisTerceiros: boolean;
+  tipoRegra: TipoRegra;
+  categoriaAutoridadeAcompanhada: CategoriaCargo | null;
+}
+
+export interface RespostaCalculoDiaria {
+  elegivel: boolean;
+  motivoIneligibilidade: string;
+  totalDias: number;
+  valorUnitarioBaseBrl: number;
+  valorTotalBrl: number;
+}
